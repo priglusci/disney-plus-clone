8f8125fedaac03dc71a56da2e00bb89151f356b9
Rezky Mandar Suaib <rezkymndr@gmail.com>
2024-08-04 20:03:33 +0800

    complete the code

--- project/src/App.js
+++ project/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
 import Header from './Components/Header';
+import Detail from './Components/Detail';
+import Home from './Components/Home';
+
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />}>
           </Route>
+          <Route path='/home' element={<Home />}>
+          </Route>
+          <Route path="/detail/:id" element={<Detail/>}></Route>
         </Routes>
       </Router>
     </div>
